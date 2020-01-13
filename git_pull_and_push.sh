9f3e0c3536361449516ae388ccb4bd81a4c68b79
翁嘉进 <1801220016@pku.edu.cn>
2020-01-13 15:01:39 +0800

    update

--- git_pull_and_push.sh
+++ git_pull_and_push.sh
@@ -1,8 +1,14 @@
 msg=$1  # $1为第一个参数
-if  [ $msg -eq "pull"]
-then
+if  [ $msg = pull ];then
 	echo "执行git pull"
     git pull
+
+elif [ $msg = update ];then
+	echo "执行git add ."
+    git add .
+	echo "执行git commit -m ${msg}"
+    git commit -m"${msg}"
+	echo "执行git push"
 else
 	echo "执行hexo generate"
     hexo g
