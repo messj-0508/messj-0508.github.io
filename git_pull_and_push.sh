acabbe8b6edee4a2beeda969ec250748cba4e1fe
翁嘉进 <1801220016@pku.edu.cn>
2020-03-26 23:55:46 +0800

    update

--- git_pull_and_push.sh
+++ git_pull_and_push.sh
@@ -9,7 +9,7 @@ elif [ $msg = update ];then
 	echo "执行git commit -m ${msg}"
     git commit -m "${msg}"
 	echo "执行git push"
-    git push
+    git push git@github.com:messj-0508/messj-0508.github.io.git hexo
 else
 	echo "执行hexo generate"
     hexo g
